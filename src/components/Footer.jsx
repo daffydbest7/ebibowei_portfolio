f9694c7ec2dd56f3546923e5af3045e39ba22454
daffybest7 <daffydbest@gmail.com>
2024-02-03 21:01:44 +0100

    footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,7 +8,7 @@ const Footer =() => {
         <div className="w-full bg-gradient-to-t from-black to-black/95"> 
             <div className="text-center min-h-[200px] text-white text-sm  lg:text-md">
                 
-                <p className="pt-12">A <a href="https://davidlawrence.onrender.com"><span className="text-teal-500"> David lawrence </span></a>Concept  &copy; 2023 </p>
+                <p className="pt-12">A <a href="https://davidlawrence.onrender.com"><span className="text-teal-500"> David lawrence </span></a>Concept  &copy; 2024 </p>
                 
             </div>
         
